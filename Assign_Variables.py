de5bdb78b313f886b3c3d98ce015e5d836f47fec
RitickMadaan <ritickmadaan55@gmail.com>
2018-09-30 08:53:45 +0530

    assigning variables

---
+++ Assign_Variables.py
@@ -0,0 +1,5 @@
+name="Ritick Madaan"
+university_roll_number=1810991129
+email="ritickmadaan55@gmail.com"
+contact="9056263665"
+print("User Details :\n","Name :",name,"\n","University Roll Number :",university_roll_number,"\n","Email :",email,"\n","Contact :",contact,)
